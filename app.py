f9e603aef219fa15b8faf5a9e4b76159b6db4d15
sfono96 <sfono96@gmail.com>
2013-11-04 16:43:30 -0700

    initial commit

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+##################### run #####################
+
+if __name__ == "__main__":
+	#app.run(debug = True, host='0.0.0.0', port=8080, passthrough_errors=True)
+	app.run(debug = True)
+
+	
+	
