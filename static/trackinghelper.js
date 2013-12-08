325101b397a98e4cf7672fad29b812180fd03608
sfono96 <sfono96@gmail.com>
2013-12-07 21:06:46 -0700

    Adding in presription base

--- static/trackinghelper.js
+++ static/trackinghelper.js
@@ -92,6 +92,20 @@ function createBarChart(chartName, chartSvg, chartData, chartTitle) {
     y.tickFormat = "%";
     y.showGridlines = false;
     s = chartName.addSeries(null, dimple.plot.bar);
+    s.addEventHandler("mouseover",function(e){if(csvg.classed("zoomedOut")){e.selectedShape.attr("fill-opacity",".3")}})
+    s.addEventHandler("mouseout",function(e){if(csvg.classed("zoomedOut")){e.selectedShape.attr("fill-opacity","1")}})
+    s.addEventHandler("click",function(e){
+        if(e.selectedShape.classed("clicked")){
+            e.selectedShape
+                .attr("fill-opacity","1")
+                .classed("clicked",false)
+        }
+        else {
+            e.selectedShape
+                .attr("fill-opacity",".3")
+                .classed("clicked",true)
+        }
+    })
     x.addOrderRule(["K","1","2","3","4","5","6"]);
     chartName.draw(1000);
 
@@ -108,7 +122,7 @@ function drawTblD(tableSvg,tableData){
     // out with the old data
     tableSvg.selectAll("g").data([]).exit().remove()
 
-    // rectangle portion
+    // row groups
     var cellHeight = 40
     var rows = tableSvg.selectAll("g")
                 .data(tableData)
@@ -117,31 +131,85 @@ function drawTblD(tableSvg,tableData){
                     .attr("transform",function(d,i){
                         return "translate("+m+", " + (cellHeight*i) + ")";
                     })
-    // rectangles/cells
+    
+    // cell groups
     var col1Width = (tblw-(m*2))/8*2 //first column width
     var colnWidth = (tblw-(m*2))/8 // other columns
-    var cells = rows.selectAll("rect")
+    var cells = rows.selectAll("g")
                     .data(function(d){return d;})
                     .enter()
-                        .append("rect")
+                        .append("g")
+                        .attr("transform",function(d,i){if(i!==0){return "translate("+(i*colnWidth+colnWidth)+",0)";}else{return "translate(0,0)";}})
+                        .on("mouseover",function(d,i,j){
+                                                        if(j===0&&i>=3){
+                                                            d3.select(this).selectAll(".roundRect").attr("fill","limegreen");
+                                                            d3.select(this).selectAll(".myText").attr("fill","white");
+                                                        }
+                                                    })
+                        .on("mouseout",function(d,i,j){
+                                                        if(j===0&&i>=3){
+                                                            d3.select(this).select(".roundRect").attr("fill","#99FF99");
+                                                            d3.select(this).select(".myText").attr("fill","black");
+                                                        }
+                                                    })
+
+                        .on("click",function(d,i,j){
+                                                        if(j===0){
+                                                            if(i===3){return prescribe('Listening','#listen','Manythings.org','ESL Lab','Esllistening.org','http://www.manythings.org/e/listening.html','http://www.esl-lab.com','http://www.esllistening.org');} // listening
+                                                            else if(i===4){return prescribe('Speaking','#speak','EslCafe.com','Eslgold.com','Eslgo.com','http://www.eslcafe.com/idea/?Speaking','http://www.eslgold.com/speaking.html','http://www.eslgo.com/resources/sa.html');} // speaking
+                                                            else if(i===5){return prescribe('Reading','#read','EslCafe.com','Eslgold.com','Eslgo.com','http://www.eslcafe.com/idea/?Speaking','http://www.eslgold.com/speaking.html','http://www.eslgo.com/resources/sa.html');} // reading
+                                                            else if(i===6){return prescribe('Writing','#write','EslCafe.com','Eslgold.com','Eslgo.com','http://www.eslcafe.com/idea/?Speaking','http://www.eslgold.com/speaking.html','http://www.eslgo.com/resources/sa.html');} // writing
+                                                        }
+                                                    })
+
+    function prescribe(modality,div,p1,p2,p3,l1,l2,l3){
+        window.$prescription = $(div).dialog({
+            title: 'Top 3 '+modality+' Interventions',
+            show: {
+                effect: "blind",
+                duration: 300
+            },
+            width: 400,
+            height: 300,
+            autoOpen: false, 
+            modal:true
+        }).css("font-family","Roboto").css("font-size","24px");
+        $prescription.html("<ol><li><a href="+l1+" target=\"_blank\">"+p1+"</a></li><li><a href="+l2+" target=\"_blank\">"+p2+"</a></li><li><a href="+l3+" target=\"_blank\">"+p3+"</a></li><ol>").dialog("open");
+    };
+  
+    var rectangles = cells.append("rect")
                         .attr("width",function(d,i){if(i===0){return col1Width;}else{return colnWidth;}})
                         .attr("height",cellHeight)
                         .attr("fill",function(d,i,j){return colorMeBaddTblD(d,i,j,benchmarks,bIdxStart)})
-                        .attr("x",function(d,i){if(i===0){return 0}else{return i * colnWidth + colnWidth;}})
+                        //.attr("x",function(d,i){if(i===0){return 0}else{return i * colnWidth + colnWidth;}})
                         .attr("fill-opacity","0")
-                        //.attr("stroke-width","1")
+                        .attr("stroke-width","1")
                         .attr("stroke","none")
-                        //.on("dblclick",animate)
-                        //.on("mouseover",function(d,i,j){if(i === 0 && j !== 0){d3.select(this).attr("fill","lightgrey")}})
-                        //.on("mouseout",function(d,i,j){if(i === 0 && j !== 0){d3.select(this).attr("fill","white")}});
+                        .attr("fill-opacity","0")
 
-    // text     
+    // then circles for the modalities
+    var roundRectangles = cells.each(function(d,i,j){
+        if(j===0&&i>=3){
+            d3.select(this).append("rect")
+                .attr("class","roundRect")
+                .attr("rx",5)
+                .attr("ry",5)
+                .attr("width",94)
+                .attr("height",37)
+                .attr("x",2)
+                //.attr("r",15)
+                .attr("fill","#99FF99")
+                .attr("fill-opacity","0")
+            .transition().duration(800)
+                .attr("fill-opacity","1")
+        }
+    }) 
+
+        // text     
     var formatter = d3.format(".1%")
-    var text = rows.selectAll("text")
-                    .data(function(d){return d;})
-                    .enter()
-                        .append("text")
-                        .attr("x",function(d,i){if(i===0){return 10;}else{return i * colnWidth + 150;}})
+    var text = cells.append("text")
+                        .attr("class","myText")
+                        .attr("x",50)
                         .attr("y",cellHeight*.65)
                         .text(function(d,i,j){if(i>1 && j !== 0){return formatter(d);}else{return d;}})
                         .style("font-family","Roboto,arial,helvetica,sans-serif")
@@ -149,12 +217,11 @@ function drawTblD(tableSvg,tableData){
                         .style("font-weight",function(d,i,j){if (i !== 0 && j !== 0){return "normal";}else{return "900";}})
                         .style("text-anchor",function(d,i){if(i!==0){return "middle";}else{return "right";}})
                         .style("font-size", function(d,i,j){if (i !== 0 && j !== 0){return "16px";}else{return "18px";}})
-                        .attr("fill-opacity","0")
     
-    cells.transition().duration(700).attr("fill-opacity","1")
+    rectangles.transition().duration(700).attr("fill-opacity","1")
     text.transition().duration(700).attr("fill-opacity","1")
 
-        };
+};
 
 function colorMeBaddTblD(d,i,j,benchmarkArr,bIdxStart){
     // we don't worry about the first row or the first 2 columns
@@ -180,7 +247,7 @@ function drawTblA(tableSvg,tableData,svgHeight,svgWidth){
     // out with the old data
     tableSvg.selectAll("g").data([]).exit().remove()
 
-    // rows
+    // rows - create group for each row
     var cellHeight = 100
     var rows = tableSvg.selectAll("g")
                 .data(tableData)
@@ -189,17 +256,45 @@ function drawTblA(tableSvg,tableData,svgHeight,svgWidth){
                     .attr("transform",function(d,i){
                         return "translate("+0+40+", " + (cellHeight*i+70) + ")";
                     })
-    // cells
+
+    // cells - create group for each cell
     var col1Width = tw/4*.9 //first column width
     var colnWidth = tw/4*.9 // other columns
-    var cells = rows.selectAll("rect")
-                    .data(function(d){return d;})
-                    .enter()
-                        .append("rect")
+    var cells = rows.selectAll("g")
+                .data(function(d){return d;}).enter()
+                    .append("g")
+                    .attr("class","notClicked")
+                    .attr("transform",function(d,i){if(i===0){return "translate(0,0)";}else{return "translate("+(i*colnWidth)+",0)";}})
+                    //.attr("x",function(d,i){if(i===0){return 0}else{return i * colnWidth;}})
+                    .on("click",function(d,i,j){if(i===0&&j!==0){
+                                        transformIt(tableSvg,atx,aty,asx,asx);
+                                        AMAOClick(d3.select(this));
+                                        appearance("b1Chrt",b1svg,b1data,"K - 6","line");
+                                        appearance("b2Chrt",b2svg,b2data,"7 - 12","line");
+                                    }
+                                })
+                    //.on("mouseover",function(d,i,j){if(i===0&&j!==0&&tableSvg.classed("zoomedOut")){d3.select(this).selectAll(".myCircle").attr("stroke","green").attr("stroke-width","3.5");}})
+                    //.on("mouseout",function(d,i,j){if(i===0&&j!==0&&tableSvg.classed("zoomedOut")){d3.select(this).selectAll(".myCircle").attr("stroke","none");}});
+                    .on("mouseover",function(d,i,j){
+                                                    if(i===0&&j!==0&&tableSvg.classed("zoomedOut")){
+                                                        d3.select(this).selectAll(".myCircle").attr("fill","limegreen")
+                                                        d3.select(this).selectAll(".aText").attr("fill","white")
+                                                    }
+                                                })
+                    .on("mouseout",function(d,i,j){
+                                                    if(i===0&&j!==0&&tableSvg.classed("zoomedOut")){
+                                                        d3.select(this).selectAll(".myCircle").attr("fill","#99FF99")
+                                                        d3.select(this).selectAll(".aText").attr("fill","grey")
+                                                    }
+                                                });
+
+    // now fill in the cells first with rectangles
+    var rectangles = cells.append("rect")
+                        //.classed("rectangle",true)
                         .attr("width",function(d,i){if(i===0){return col1Width;}else{return colnWidth;}})
                         .attr("height",cellHeight)
                         .attr("fill",function(d,i,j){return colorMeBaddTblA(d,i,j,benchmarks,bIdxStart)})
-                        .attr("x",function(d,i){if(i===0){return 0}else{return i * colnWidth;}})
+                        //.attr("x",function(d,i){if(i===0){return 0}else{return i * colnWidth;}})
                         //.attr("fill-opacity","0")
                         .attr("stroke","white")
                         .attr("stroke-width",2)
@@ -207,13 +302,24 @@ function drawTblA(tableSvg,tableData,svgHeight,svgWidth){
                         //.on("mouseover",function(d,i,j){if(i === 0 && j !== 0){d3.select(this).attr("fill","lightgrey")}})
                         //.on("mouseout",function(d,i,j){if(i === 0 && j !== 0){d3.select(this).attr("fill","white")}});
 
-    // text     
+    // then circles for the AMAO's
+    var circles = cells.each(function(d,i,j){
+        if(i===0&&j!==0){
+            d3.select(this).append("circle")
+                .attr("class","myCircle")
+                .attr("cx",78.5)
+                .attr("cy",51)
+                .attr("r",40)
+                .attr("fill","#99FF99")
+                .attr("fill-opacity","1")
+        }
+    })
+
+    // then text     
     var formatter = d3.format(".1%")
-    var text = rows.selectAll("text")
-                    .data(function(d){return d;})
-                    .enter()
-                        .append("text")
-                        .attr("x",function(d,i){if(i===0){return i * colnWidth + 80;}else{return i * colnWidth + 80;}})
+    var text = cells.append("text")
+                        .attr("class","aText")
+                        .attr("x",80)
                         .attr("y",cellHeight*.65)
                         .text(function(d,i,j){if(i>bIdxStart-1 && j !== 0){return formatter(d);}else{return d;}})
                         .style("font-family","Roboto,arial,helvetica,sans-serif")
@@ -221,11 +327,10 @@ function drawTblA(tableSvg,tableData,svgHeight,svgWidth){
                         .style("font-weight",function(d,i,j){if (i !== 0 && j !== 0){return "normal";}else{return "900";}})
                         .style("text-anchor","middle")
                         .style("font-size", function(d,i,j){if (i !== 0 && j !== 0){return "30px";}else{return "35px";}})
-                        //.attr("fill-opacity","0")
 
-    //tableSvg.transition().duration(1000).attr("fill-opacity","1")
-    
-        };
+
+};
+
 
 function colorMeBaddTblA(d,i,j,benchmarkArr,bIdxStart){
     // we don't worry about the first row or the first 2 columns
@@ -348,3 +453,16 @@ function filter(json,district){
 
     return dataset;
 };
+
+function AMAOClick(selection){
+    if(selection.classed("notClicked")){
+        selection.selectAll(".myCircle").attr("fill","limegreen");
+        selection.selectAll(".aText").attr("fill","white");
+        selection.classed("notClicked",false);
+    }
+    else {
+        selection.selectAll(".myCircle").attr("fill","#99FF99");
+        selection.selectAll(".aText").attr("fill","black");
+        selection.classed("notClicked",true);
+    }
+}
