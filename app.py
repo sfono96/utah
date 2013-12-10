f55a53d95a57efad4c1cb7bc0c557dafbc238a78
sfono96 <sfono96@gmail.com>
2013-12-09 20:45:36 -0700

    added login page

--- app.py
+++ app.py
@@ -1,8 +1,47 @@
 from flask import *
+from functools import wraps
+
+#################### app ######################
 
 app = Flask(__name__)
+app.secret_key = '5RD{Hd1CRSwCoqct4Y497&4Ar12t5V'
+
+# login decorator
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to login first dude.')
+			return redirect(url_for('log'))
+	return wrap
+
+################## routes #####################
+
+# logout session
+@app.route('/logout')
+def logout():
+	session.pop('logged_in', None)
+	flash('You were logged out')
+	return redirect(url_for('log'))
+
+# login
+@app.route('/log',methods=['GET','POST'])
+def log():
+	error = None
+	if request.method == 'POST':
+		if request.form['uid'] != 'admin' or request.form['pwd'] != 'fateahu77':
+			error = 'Invalid Credentials. Please try again.'
+			flash(error)
+			return redirect(url_for('log'))
+		else:
+			session['logged_in'] = True
+			return redirect(url_for('home'))
+	return render_template('login.html',error=error)
 
 @app.route('/')
+@login_required
 def home():
 	return render_template('index.html')
 
